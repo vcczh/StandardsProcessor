7b5a38cea0cbe3723294218c7ab7c9e4951236e5
VictorCZH <victorchuama@gmail.com>
2014-04-17 11:26:13 -0700

    StandardsProcessor
    a program that will parse XML content created by the Common Core State
    Standards Initiative

---
+++ src/main/java/com/chusproject/StandardsProcessor.java
@@ -0,0 +1,13 @@
+package com.chusproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class StandardsProcessor
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Edlio!" );
+    }
+}
